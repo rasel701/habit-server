49c5bc4daee0c6d731b5c0206a8a0c2874530869
rasel <rasel708211@gmail.com>
2025-11-10 06:36:39 -0800

    All habit info, hait add , and my habit route is created

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello This is a habit tracker !");
+});
+
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8tne59p.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -21,8 +28,30 @@ async function run() {
     const db = client.db("habit-tracker-db");
     const habitCollection = db.collection("habit-info");
 
-    app.get("/habit-info", (req, res) => {
-      res.send("habit-information");
+    app.get("/habit-info", async (req, res) => {
+      const result = await habitCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/habit-info", async (req, res) => {
+      const newHabit = req.body;
+      const result = await habitCollection.insertOne(newHabit);
+      res.send(result);
+    });
+
+    app.get("/my-habit", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+      try {
+        const result = await habitCollection.find(query).toArray();
+        res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
@@ -35,13 +64,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello This is a habit tracker !");
-});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
