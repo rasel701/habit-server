79f13060c0dc86ef746579e09041921b0f49ffdb
rasel <rasel708211@gmail.com>
2025-11-10 18:16:43 -0800

    create habit-latest api and single habit api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = 3000;
@@ -33,6 +33,30 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/habit-latest", async (req, res) => {
+      const result = await habitCollection
+        .find()
+        .sort({ createAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/habit-info/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {};
+      if (id) {
+        query._id = new ObjectId(id);
+      }
+      try {
+        const result = await habitCollection.findOne(query);
+        res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
+
     app.post("/habit-info", async (req, res) => {
       const newHabit = req.body;
       const result = await habitCollection.insertOne(newHabit);
@@ -47,6 +71,41 @@ async function run() {
       }
       try {
         const result = await habitCollection.find(query).toArray();
+        res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
+
+    app.patch("/habit-info/:id", async (req, res) => {
+      const id = req.params.id;
+      const newData = req.body;
+      const query = {};
+      if (id) {
+        query._id = new ObjectId(id);
+      }
+      const update = {
+        $set: newData,
+      };
+      try {
+        const result = await habitCollection.updateOne(query, update);
+        res.status(200).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+      }
+    });
+
+    app.delete("/habit-info/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {};
+      if (id) {
+        query._id = new ObjectId(id);
+      }
+
+      try {
+        const result = await habitCollection.deleteOne(query);
         res.status(200).json(result);
       } catch (error) {
         console.error(error);
